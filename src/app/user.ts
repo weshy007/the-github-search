df61e2760452444c3cff9a8d0ec5f500408b2c1e
weshy007 <josephwaweru96@gmail.com>
2021-02-08 11:26:58 +0300

    Reduced the cmponents

--- src/app/user.ts
+++ src/app/user.ts
@@ -3,8 +3,6 @@ export class User {
     constructor (
         public name:string,
         public login: string,
-        public comapany: string,
-        public bio: string,
         public followers: number,
         public following: number,
         public repos_url: string,
