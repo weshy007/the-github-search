e213a045202144efaa71c17f761a1c6cd57d1051
weshy007 <josephwaweru96@gmail.com>
2021-02-08 19:08:21 +0300

    added the router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GitDisplayComponent } from './git-display/git-display.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: 'git-display', component: GitDisplayComponent},
+  { path: 'landingpage', component: LandingpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
